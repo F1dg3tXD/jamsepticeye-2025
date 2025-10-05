6a4decbb6021b08baff26bddea3831619909900f
F1dg3t <57208077+F1dg3tXD@users.noreply.github.com>
2025-10-04 18:32:40 -0700

    Add entrance door.

--- models/checkpoint.gd
+++ models/checkpoint.gd
@@ -2,6 +2,11 @@ extends Node3D
 
 @onready var area_3d: Area3D = $Area3D
 @onready var checkpoint_spawn: Node3D = $checkpoint_spawn
+@onready var checkpoint_text: Label3D = $checkpointText
+@export var rotation_speed: float = 0.01 # Degrees per second
+
+func _process(delta: float) -> void:
+	checkpoint_text.rotate_z(rotation_speed)
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body.is_in_group("player_pawn"):
